72cf34ca79db7cb00412f72d33bb279e0d2cbb41
santiago-restrepo <sanpra1208@gmail.com>
2021-02-26 17:40:40 -0500

    add basic project structure

---
+++ src/main/java/com/redtutores/backend/RedTutoresBackendApplication.java
@@ -0,0 +1,13 @@
+package com.redtutores.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedTutoresBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedTutoresBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/redtutores/backend/RedTutoresBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.redtutores.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedTutoresBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
